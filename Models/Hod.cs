c9f1bf6b68d2209247fc8736e9a13e2d8f2021b1
AnirbanPa1 <palanirban2003@gmail.com>
2023-09-19 18:48:29 +0530

    idk

---
+++ Models/Hod.cs
@@ -0,0 +1,6 @@
+public class Hod{
+    public string name { get; set; }
+    public int d_code { get; set; } //foriegn key
+    public string d_name { get; set; }
+    public int college_code { get; set; } //foriegn key
+}

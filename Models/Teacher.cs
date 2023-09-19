257b0d01792a6825161a47a42ec422bc6788362f
AnirbanPa1 <palanirban2003@gmail.com>
2023-09-19 21:53:20 +0530

    idk

--- Models/Teacher.cs
+++ Models/Teacher.cs
@@ -1,11 +1,18 @@
+using System.ComponentModel.DataAnnotations.Schema;
+using hexahack.Models;
 using Microsoft.AspNetCore.Identity;
 
-public class Teacher: IdentityUser {
-    public string teacher_name { get; set; } 
-    public string email { get; set; }
-    public int college_code { get; set; } // foriegn key
-    public string subject { get; set; }
-    public int hashed_password { get; set; }
-    public int d_code { get; set; } // foriegn key
+namespace hexahack.Models{
+    public class Teacher: IdentityUser {
+        public string teacher_name { get; set; } 
+        public string email { get; set; }
+        [ForeignKey("College")]
+        public int college_code { get; set; } // foriegn key
+        public string subject { get; set; }
+        public int hashed_password { get; set; }
+        [ForeignKey("Department")]
+        public int d_code { get; set; } // foriegn key
+        public College College { get; set; }
+        public Department Department { get; set; }
+    }
 }
-
